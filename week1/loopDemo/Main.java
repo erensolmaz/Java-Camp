e00f3cf2c759d81e053ea2f9bd214864c592f7ef
Eren <40443383+erensolmaz@users.noreply.github.com>
2022-09-22 02:59:42 +0300

    week1

---
+++ week1/loopDemo/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+		for(int i=2;i<10;i+=2){
+			System.out.println(i);
+			
+		}
+		System.out.println("For dongusu bitti");
+
+		int i=2;
+		while(i<10){
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("While dongusu bitti");
+
+		int j=100;
+		do{
+			System.out.println(j);
+			j+=2;
+		}while(j<10);
+		System.out.println("DO while dongusu bitti");
+	}
+}
