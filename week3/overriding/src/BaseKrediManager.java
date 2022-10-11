7ac68474b53e76dcace5187092c314919a38d365
Eren <40443383+erensolmaz@users.noreply.github.com>
2022-10-11 20:50:30 +0300

    Delete week3/overriding directory

--- week3/overriding/src/BaseKrediManager.java
+++
@@ -1,7 +0,0 @@
-public class BaseKrediManager {
-    public /*"final" kullanırsak override engellenir*/ double hesapla(double tutar){
-        return tutar  * 1.18;
-
-    }
-    
-}
--- week3/overriding/src/OgretmenKrediManager.java
+++
@@ -1,4 +0,0 @@
-public class OgretmenKrediManager extends BaseKrediManager {
-
-    
-}
