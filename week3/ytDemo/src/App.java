8948934321b93dffb0a323489536e926d868dbf4
Noiren <solmazeren2009@hotmail.com>
2022-10-12 16:22:11 +0300

    week1/updated

---
+++ week3/ytDemo/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        CustomerManager customerManager = new CustomerManager(new Customer(), new TeacherCreditManager());
+        customerManager.GiveCredit();
+        TeacherCreditManager teacherCreditManager = new TeacherCreditManager();
+        teacherCreditManager.save();
+
+    }
+}
