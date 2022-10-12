5c12aca4b499d208490e26db2b83f2bb66dfe247
Eren <40443383+erensolmaz@users.noreply.github.com>
2022-10-12 16:18:35 +0300

    Delete week3/YtDemo directory

--- week3/YtDemo/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        CustomerManager customerManager = new CustomerManager(new Customer(), new TeacherCreditManager());
-        customerManager.GiveCredit();
-        TeacherCreditManager teacherCreditManager = new TeacherCreditManager();
-        teacherCreditManager.Save();
-
-    }
-}
